8f23d00d86e2fd6015073d71e411c8e94a56ceb7
Coline Peetermans <colinepeetermans@macbook-pro.home>
2022-05-03 11:29:02 +0200

    about page

--- src/components/templates/AboutTemplate.vue
+++ src/components/templates/AboutTemplate.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="about">
     <Transition name="slide-project">
-    <div class="flex" v-if="reveal">
-      <div class="picture">
+      <div class="flex" v-if="reveal">
+        <!-- <div class="picture">
         <img
           src="https://scontent.fbru4-1.fna.fbcdn.net/v/t39.30808-6/278670378_10160820934025839_5985026310559284514_n.jpg?stp=dst-jpg_s526x395&_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=pRk4jBodAqMAX_C9U-J&_nc_ht=scontent.fbru4-1.fna&oh=00_AT_wsiVVv-gHl1iXG9v142NTZcUhCQfeJyoVDsT2ORIqYQ&oe=627345B1"
           alt=""
         />
+      </div> -->
+        <div class="text">
+          <p>
+            I am a front-end developer with a true passion for digital
+            experiences. I have more than 3 years of experience in designing and
+            building web projects such as showcase sites, wordpress themes and
+            platforms.
+          </p>
+          <button class="call-to-action">
+            <span><i class="fas fa-arrow-right"></i> Contact</span>
+          </button>
+        </div>
+        <div class="background-element"></div>
       </div>
-      <div class="text">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
-          libero non labore cum dolores eum voluptates error possimus expedita,
-          numquam iusto ut incidunt nostrum, suscipit quis. Hic laudantium
-          repellat blanditiis!
-        </p>
-        <button class="call-to-action"><span><i class="fas fa-arrow-right"></i> Contact</span></button>
-      </div>
-      <div class="background-element"></div>
-    </div>
     </Transition>
   </div>
 </template>
