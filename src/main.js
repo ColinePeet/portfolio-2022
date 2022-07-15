3ffe14df885fa8c29669b0613b95516ac3413478
Coline Peetermans <colinepeetermans@MacBook-Pro-de-Coline.local>
2022-07-15 16:54:10 +0200

    fix mobile responsive issues

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 createApp(App).use(store).use(router).mount('#app')
 
