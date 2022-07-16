da8236c9c19a387699b2c85aa0ec70e5e5659c52
Coline Peetermans <colinepeetermans@MacBook-Pro-de-Coline.local>
2022-07-16 14:07:55 +0200

    project added and video compressed

--- src/store/projects.js
+++ src/store/projects.js
@@ -5,7 +5,9 @@ import antartica_1 from "@/assets/img/antartica_1.mp4";
 import antartica_2 from "@/assets/img/antartica_2.mp4";
 import antartica_3 from "@/assets/img/antartica_3.mp4";
 import weekday from "@/assets/img/weekday.mp4";
-
+import follaw_1 from "@/assets/img/follaw_1.mp4";
+import follaw_2 from "@/assets/img/follaw_2.mp4";
+import follaw_3 from "@/assets/img/follaw_3.mp4";
 
 const projects = [
   {
@@ -16,6 +18,15 @@ const projects = [
     repo_link: "https://github.com/ColinePeet/martine",
     background_color: '#c9dae5',
     description: "Simple showcase website for the belgian painter Martine Durt to show her latest paintings and bio. Personal project designed and built with HTML5, Bootstrap, CSS3/Sass and JavaScript/jQuery.",
+  },
+  {
+    title: "Follaw.sv",
+    type: "platform",
+    image: [follaw_1, follaw_2, follaw_3],
+    website_link: "https://www.saper-vedere.eu/follaw-sv-un-outil-de-monitoring-pour-la-veille-legislative-et-la-veille-institutionnelle/",
+    repo_link: null,
+    background_color: '#d2daf7',
+    description: "Follaw is a monitoring platform for all public affairs issues. Platform developed as lead front-end at Saper Vedere with the team of developers. Made with the framework Vue.js",
   },
   {
     title: "Hors-Champs",
@@ -44,8 +55,6 @@ const projects = [
     background_color: '#dce3cd',
     description: "Newsletter of a typical 'weekly edit' of the Weekday brand,  which puts a selection of items in value. Project designed and developed as a training exercise with HTML5.",
   },
-
-
 ]
 
 
